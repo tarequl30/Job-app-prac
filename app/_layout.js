68051bfd5293645c626d14139025802bfe3a7a64
Tareq <tarek@Tareks-MacBook-Pro.local>
2023-03-29 00:13:42 +0600

    add something more

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import {useFonts} from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 
+
+ 
 export default function Layout() {
   return <Stack />;
 }
--- app/index.js
+++ app/index.js
@@ -1,20 +1,41 @@
-import { View, Text , SafeAreaView} from "react-native";
+import { View, Text , SafeAreaView, Dimensions, ScrollViewComponent} from "react-native";
 import { useState } from "react";
 import { Stack, stack, useRouter} from 'expo-router'
 
 import { COLORS, icons, images, SIZES} from '../constants'
-import { Nearbyjobs, popularjobs, screenHeaderBtn, Welcome} from '../components'
+import { Nearbyjobs, Popularjobs, popularjobs, ScreenHeaderBtn, screenHeaderBtn, Welcome} from '../components'
+import { ScrollView } from "react-native-gesture-handler";
 
 const Home = () => {
   const router = useRouter()
   return(
-    <SafeAreaView style={{ flex : 1, backgroundColor: COLORS , lightWhite}}> 
+    <SafeAreaView style={{ flex : 1, backgroundColor: COLORS.lightWhite}}> 
     <Stack.Screen 
     options={{
       headerStyle : {backgorundColor: COLORS.lightWhite},
+      headerShadowVisible: false,
+      headerLeft: () => (
+        <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+      ),
+      headerRight: () => (
+        <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+      ),
+      headerTitle: ''
     }}
     />
-      <Text>Hello</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View 
+      style={{
+        flex: 1,
+        padding: SIZES.medium
+      }}
+      >
+     <Welcome />
+     <Popularjobs />
+     <Nearbyjobs />
+      </View>
+
+    </ScrollView>
        </SafeAreaView>
   )
 }
